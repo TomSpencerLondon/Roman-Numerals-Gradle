3df49284f40b5e1a6e8d9fb2a9a4f70f0d8a075d
Tom <thomas.spencer@codurance.com>
2021-06-29 17:37:26 +0100

    Refactor for convert:

--- src/main/java/com/codurance/RomanNumeral.java
+++ src/main/java/com/codurance/RomanNumeral.java
@@ -5,17 +5,11 @@ import java.util.*;
 public class RomanNumeral {
 
   public String convert(int arabic) {
-    Map<Integer, String> numerals = new HashMap<>();
-    numerals.put(1, "I");
-    numerals.put(4, "IV");
-    numerals.put(5, "V");
-    numerals.put(6, "VI");
-    numerals.put(9, "IX");
-    numerals.put(10, "X");
+    Map<Integer, String> numerals = getNumeralConversions();
 
     StringBuilder result = new StringBuilder();
     int number = arabic;
-    for (int n : getNums(numerals)) {
+    for (int n : getNums()) {
       while (n <= number) {
         result.append(numerals.get(n));
         number -= n;
@@ -24,8 +18,19 @@ public class RomanNumeral {
     return result.toString();
   }
 
-  private List<Integer> getNums(Map<Integer, String> numerals) {
-    List<Integer> nums = new ArrayList<>(numerals.keySet());
+  private Map<Integer, String> getNumeralConversions() {
+    Map<Integer, String> numerals = new HashMap<>();
+    numerals.put(1, "I");
+    numerals.put(4, "IV");
+    numerals.put(5, "V");
+    numerals.put(6, "VI");
+    numerals.put(9, "IX");
+    numerals.put(10, "X");
+    return numerals;
+  }
+
+  private List<Integer> getNums() {
+    List<Integer> nums = new ArrayList<>(getNumeralConversions().keySet());
     Collections.reverse(nums);
     return nums;
   }
